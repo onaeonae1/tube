42cd2ba8c10af347909c9b68a7cb53440366c355
onaeonae1 <41908963+onaeonae1@users.noreply.github.com>
2020-06-18 23:32:53 +0900

    middleware test

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log('hello');
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser"; 
+
+const app = express();
+
+const handleListening = () => console.log("4000 working");
+const handleHome = (req, res) => res.send("handle home");
+const handleProfile = (req,res) => res.send("you are on my profile");
+
+//middlewares
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(helmet());
+app.use(morgan("dev"));
+
+//////routes 
+app.get('/',  handleHome);
+app.get('/profile',handleProfile);
+
+///
+app.listen(4000,handleListening);
