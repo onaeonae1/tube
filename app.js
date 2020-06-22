267b34bc9662b6e799ffd515c6e4cf4e3b4de7e6
onaeonae1 <41908963+onaeonae1@users.noreply.github.com>
2020-06-22 13:36:02 +0900

    routers, controllers

---
+++ app.js
@@ -0,0 +1,29 @@
+import express, { Router } from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser"; 
+
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter"; 
+import globalRouter from "./routers/globalRouter";
+import routes from "./routes.js";
+
+const app = express();
+
+
+//middlewares
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(helmet());
+app.use(morgan("dev"));
+
+//////routes 
+app.use(routes.videos,videoRouter);
+app.use(routes.home,globalRouter);
+app.use(routes.users,userRouter);
+
+///
+
+export default app;
---
+++ routers/globalRouter.js
@@ -0,0 +1,20 @@
+import express from "express";
+import routes from "../routes";
+import { home, search } from "../controllers/videoController";
+import {logout, login, join } from "../controllers/userController";
+const globalRouter = express.Router();
+
+globalRouter.get(routes.home,home);
+globalRouter.get(routes.search,search);
+globalRouter.get(routes.search,search);
+globalRouter.get(routes.join, join);
+globalRouter.get(routes.login, login);
+globalRouter.get(routes.logout,logout);
+
+
+
+export default globalRouter;
+
+
+
+
