1dd6e83eedae5cca42f7aa674e182a76d2adc545
onaeonae1 <41908963+onaeonae1@users.noreply.github.com>
2020-07-22 23:23:29 +0900

    Prestart

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+import "@babel/polyfill";
+import path from "path";
 import express from "express";
 import morgan from "morgan";
 import helmet from "helmet";
@@ -21,8 +23,8 @@ const CokieStore = MongoStore(session);
 
 app.use(helmet());
 app.set("view engine", "pug");
-app.use("/uploads", express.static("uploads"));
-app.use("/static", express.static("static"));
+app.set("views", path.join(__dirname, "views"));
+app.use("/static", express.static(path.join(__dirname, "static")));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
